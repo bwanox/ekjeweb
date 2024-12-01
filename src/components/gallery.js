79172bbfd93d7b47ed2738cefd929f413e2a3ef3
bwanox <moad.bil7458@gmail.com>
2024-12-01 18:58:01 +0100

    commit

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -7,28 +7,32 @@ import pic4 from "../assets/pic4.png";
 
 const Gallery = () => {
   const images = [
-    pic1, pic2, pic3, pic4,
-    "https://images6.alphacoders.com/749/thumb-1920-749966.png",
-    "https://images4.alphacoders.com/761/thumb-1920-761076.png",
-    "https://images.alphacoders.com/682/thumb-1920-682570.png",
-    "https://images4.alphacoders.com/866/thumb-1920-866812.png",
+    { src: pic1, alt: "Highlight 1" },
+    { src: pic2, alt: "Highlight 2" },
+    { src: pic3, alt: "Highlight 3" },
+    { src: pic4, alt: "Highlight 4" },
+   
   ];
 
   return (
     <div className="gallery-container">
       <div className="gallery-text-container">
-        <h1 className="gallery-title">
-          highlights
-        </h1>
+        <h1 className="gallery-title">Highlights</h1>
       </div>
-    <div className="gallery">
-      
-      {images.map((src, index) => (
-        <span key={index} style={{ "--i": index + 1 }}>
-          <img src={src} alt={`Gallery Image ${index + 1}`} />
-        </span>
+      <div className="gallery">
+      {images.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} />
       ))}
-    </div>
+      {/*<ul className="gallery-ul">
+        {images.map((image, index) => (
+          <li className ='gallery-li' key={index}>
+            <a className="gallery-a" href={image.link}>
+              <img className='gallery-img' src={image.src} alt={image.alt} />
+            </a>
+          </li>
+        ))}
+      </ul>*/}
+      </div>
     </div>
   );
 };
