0db2f8fba8ec5985009aed9d3a9e6980d2f65825
bwanox <moad.bil7458@gmail.com>
2024-12-02 10:03:40 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const App = () => {
 
   return (
     <Router>
-      <div className="app-container">
+      <div className="app-con tainer">
         <Routes>
           <Route
             path="/"
